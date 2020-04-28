39ea14159c4d10ca1b6229c632ef0ad581cd3421
Jaemin Ahn <jaemin@pozalabs.com>
2020-04-28 18:38:51 +0900

    Feature/typescript (#3)
    * fea)typescript 설치
    
    * fea) typescript를 사용하기위해 webpack, babel설정
    
    * fea)typescript로 소스코드 포팅

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-const REACT_PROJECT_ENTRY_PATH = path.resolve(__dirname, './src/index.js');
+const REACT_PROJECT_ENTRY_PATH = path.resolve(__dirname, './src/index.tsx');
 
 module.exports = {
   entry: REACT_PROJECT_ENTRY_PATH,
@@ -9,10 +9,13 @@ module.exports = {
     path: path.resolve(__dirname, './.dist'),
     filename: 'index_bundle.js'
   },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|ts)x?$/,
         exclude: /node_module/,
         use: {
           loader: 'babel-loader'
