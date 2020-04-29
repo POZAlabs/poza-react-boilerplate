2facd1d50ec2eef55b7ab18efde401457a11e6e8
Jaemin Ahn <jaemin@pozalabs.com>
2020-04-29 11:40:28 +0900

    Feature/parsing image files (#6)
    * fea)Assets를 번들링 하기위한 라이브러리 설치
    
    * fea)Webpack에 번들링 로더 설정
    
    * fea)SVG 파싱을위한 설정
    
    * fea)테스트용  svg파일 추가

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "../assets/svgs/logo.svg";
 
 const App: React.FC = () => {
-  return <Wrapper>This is Demo</Wrapper>;
+  return (
+    <Wrapper>
+      <h1>This is Demo</h1>
+      <Logo />
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled.h1``;
+const Wrapper = styled.div``;
 
 export default App;
---
+++ src/types/svg.d.ts
@@ -0,0 +1,4 @@
+declare module "*.svg" {
+  const SvgElement: React.ComponentType<React.SVGProps<SVGElement>>;
+  export = SvgElement;
+}
